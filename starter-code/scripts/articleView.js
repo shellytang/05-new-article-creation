df8b89cecc53a3c5779a6d9585aba6d667d8eeec
Shelly Tang <shellytang@gmail.com>
2017-01-06 14:36:27 -0800

    instantiate article based on value from form fields

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -66,18 +66,32 @@ articleView.setTeasers = function() {
 
 articleView.initNewArticlePage = function() {
   // Done: Ensure the main .tab-content area is revealed. We might add more tabs later.
-    articleView.handleMainNav();
-  // TODO: The new articles we create will be copy/pasted into our source data file.
+    // articleView.handleMainNav();
+    $('main .tab-content').show();
+  // Done: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide the export field for now, and show it once we have data to export.
-
+  $('#export-field').hide();
   // TODO: Add an event listener/handler to update the preview and the export field if any inputs change.
+  $('#articles').on('change', function() {
+    // $(this).  get bakc here
+
+  });
 };
 
 articleView.create = function() {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-
+  var newArt;
+  $('#articles').empty();
   // TODO: Instantiate an article based on what's in the form fields:
+  var newArt = new Article({
+    title: $('#article-title').val(),
+    body: $('#article-body').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-url').val(),
+    category: $('#article-category').val(),
+    published: $('#article-published').is(':checked'),
+  })
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
 
